273bbfdf41c5955d0be056f6bc204db394c9654f
captivecow <captivecow.dev@gmail.com>
2025-03-23 21:57:00 -0500

    chore: remove dep

--- client/src/main/java/io/github/captivecow/ChatScreen.java
+++ client/src/main/java/io/github/captivecow/ChatScreen.java
@@ -7,7 +7,6 @@ import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
-import io.github.captivecow.shared.Connect;
 
 public class ChatScreen implements Screen {
 
